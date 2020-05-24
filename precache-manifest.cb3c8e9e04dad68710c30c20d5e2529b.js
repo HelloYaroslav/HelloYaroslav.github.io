5da035157d43b1a111008887cce501ab98362126
yaroslav <yaroslav.klimenkovv@gmail.com>
2020-05-24 18:43:37 +0300

    buld v 0.1.0

---
+++ precache-manifest.cb3c8e9e04dad68710c30c20d5e2529b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cce58599009a6f79eec9f5eeb617ac4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45a9d3f5f339d29ed282",
+    "url": "/static/css/2.4c6ee7a4.chunk.css"
+  },
+  {
+    "revision": "c756dd520d9e8dbbe9a0",
+    "url": "/static/css/main.3f02f951.chunk.css"
+  },
+  {
+    "revision": "45a9d3f5f339d29ed282",
+    "url": "/static/js/2.414dde09.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.414dde09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c756dd520d9e8dbbe9a0",
+    "url": "/static/js/main.cb136f56.chunk.js"
+  },
+  {
+    "revision": "24b0298cc1b05b9e966e",
+    "url": "/static/js/runtime-main.a351475d.js"
+  },
+  {
+    "revision": "d998cee4ab5d8f316932ffd1a0dfe496",
+    "url": "/static/media/benderBoldItalic.d998cee4.otf"
+  },
+  {
+    "revision": "c07bd65f4da44889e0c4017acad134ca",
+    "url": "/static/media/manager.c07bd65f.png"
+  }
+]);
